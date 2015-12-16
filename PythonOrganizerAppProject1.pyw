825f5cd40a50f83606e9307661989b659984bc22
karadalex <karadalex@gmail.com>
2015-12-16 03:49:48 +0200

    Initial commit

---
+++ PythonOrganizerAppProject1.pyw
@@ -0,0 +1,5 @@
+#import PythonOrganizerAppProject1 module
+import PythonOrganizerAppProject1
+
+#Run the main function of the program
+PythonOrganizerAppProject1.run_main()
---
+++ csvViewer.py
@@ -0,0 +1,37 @@
+import wx
+import os
+
+def makeContactsTextFromCsv():
+    app = wx.PySimpleApp()
+    wildcard = "Outlook CSV file (*.csv)|*.csv|" \
+               "Gmail CSV file (*.csv)|*.csv|" \
+            "All files (*.*)|*.*"
+    dialog = wx.FileDialog(None, "Choose a file", os.getcwd(), "", wildcard, wx.OPEN)
+    if dialog.ShowModal() == wx.ID_OK:
+        filePath = dialog.GetPath()
+        print "Openning: ", filePath, "..."
+        print "_________________________________________________________________________________"
+    dialog.Destroy()
+
+    csvFile = open(filePath, "r")
+    newFile = open("contactsFile.txt", "a+")
+    csvFileArray = []
+    for i in csvFile:
+        csvFileArray.append(i.split(","))
+    print csvFileArray
+
+    tagsList = csvFileArray[0]
+    for i in range(1, len(csvFileArray)):
+        for j in range(len(csvFileArray[0])):
+            if csvFileArray[i][j] !='' and csvFileArray[i][j] != '\n':
+                newFile.write(csvFileArray[0][j] + ": " + csvFileArray[i][j])
+                newFile.write('\n')
+        newFile.write("_________________________________________________________________________________")
+        newFile.write('\n')
+
+    csvFile.close()
+    newFile.close()
+    return newFile
+
+newFile = makeContactsTextFromCsv()
+
